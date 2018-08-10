f6f2cbcda8f20b686719d322940cef7e5a32aa2e
Patrick Schaefer <pschaefer@minnow-environmental.com>
2018-08-10 15:36:48 -0400

    update

--- Create_taxonomies.R
+++ Create_taxonomies.R
@@ -52,7 +52,23 @@ for (i in names(rev(taxa.list))){
 }
 
 taxa$count<-ceiling(rlnorm(nrow(taxa),5,2))
+taxa$full.taxon<-apply(taxa[,colnames(taxa)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")],
+                       1,paste0,collapse="-")
 
+ref.out<-t(as.numeric(taxa$count))
+colnames(ref.out)<-taxa$full.taxon
+
+ref.coms<-list()
+for (x in 1:30){
+  ref.coms[[x]]<-vegan::rrarefy(ref.out,500)
+}
+
+ref.df<-data.frame(matrix(unlist(ref.coms), nrow=length(taxa$full.taxon), byrow=F))
+
+rownames(ref.df)<-taxa$full.taxon
+ref.df<-data.frame(t(ref.df))
+
+#lapply(ref.coms,t)
 ####################
 #  remove taxa
 
@@ -67,77 +83,115 @@ names(checked)<-c("n.p","n.c","n.o","n.f","n.g","n.s")
 
 res.ratio<-t(apply(res.ratio,1,cumsum))
 
-taxa.whole<-taxa
+rem.coms<-list()
 
-for (i in names(rev(taxa.list))) {
-  if (i=="n.s"){
-    for (n in unique(taxa[,i])) {
-      decision<-round(runif(1,min=1,max=sum(res.ratio[i,])))
-      if (decision<=res.ratio[i,"all.ID"]){
-        next
-      } else if (decision<=res.ratio[i,"non.ID"]){
-        new.tax<-taxa[taxa[,i]==n,]
-        new.tax[,i]<-"NA"
-        taxa<-rbind(taxa,new.tax)
-        taxa<-taxa[taxa[,i]!=n,]
-      } else { #some.IF
-        numb.kept<-ceiling(runif(1,0.1,0.99)*taxa$count[taxa[,i]==n])
-        numb.upped<-taxa$count[taxa[,i]==n]-numb.kept
-        new.tax<-taxa[taxa[,i]==n,]
-        new.tax[,i]<-"NA"
-        new.tax$count<-numb.upped
-        taxa$count[taxa[,i]==n]<-numb.kept
-        taxa<-rbind(taxa,new.tax)
+for (x in 1:30){
+  taxa<-t(ref.coms[[x]])
+  taxa<-cbind(rownames(taxa),taxa)
+  colnames(taxa)<-c("full.taxon","count")
+  taxa<-data.frame(taxa)
+  taxa<-cbind(taxa,do.call(rbind, strsplit(as.character(taxa$full.taxon),"-")))
+  colnames(taxa)[3:8]<-c("n.p","n.c","n.o","n.f","n.g","n.s")
+  taxa[c(1,3:8)]<-apply(taxa[c(1,3:8)],2,as.character)
+  taxa$count<-as.numeric(as.character(taxa$count))
+
+    for (i in names(rev(taxa.list))) {
+    if (i=="n.s"){
+      for (n in unique(taxa[,i])) {
+        if (taxa$count[taxa$n.s==n]==0) next
+        
+        decision<-round(runif(1,min=1,max=sum(res.ratio[i,])))
+        if (decision<=res.ratio[i,"all.ID"]){
+          next
+        } else if (decision<=res.ratio[i,"non.ID"]){
+          new.tax<-taxa[taxa[,i]==n,]
+          new.tax[,i]<-"NA"
+          taxa<-rbind(taxa,new.tax)
+          taxa<-taxa[taxa[,i]!=n,]
+        } else { #some.IF
+          numb.kept<-ceiling(runif(1,0.1,0.99)*taxa$count[taxa[,i]==n])
+          numb.upped<-taxa$count[taxa[,i]==n]-numb.kept
+          new.tax<-taxa[taxa[,i]==n,]
+          new.tax[,i]<-"NA"
+          new.tax$count<-numb.upped
+          taxa$count[taxa[,i]==n]<-numb.kept
+          taxa<-rbind(taxa,new.tax)
+        }
       }
-    }
-    taxa$full.taxon<-apply(taxa[,colnames(taxa)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")],
-                           1,paste0,collapse="-")
-    taxa1<-aggregate(count~full.taxon,data=taxa,sum)
-    taxa<-cbind(taxa1,do.call(rbind, strsplit(taxa1$full.taxon,"-")))
-    colnames(taxa)[3:8]<-c("n.p","n.c","n.o","n.f","n.g","n.s")
-    taxa[3:8]<-apply(taxa[3:8],2,as.character)
-    checked[i]<-T
-  } else {
-    for (n in unique(
-      taxa$full.taxon[taxa[,names(checked[which.max(checked)])]=="NA"]
-    )
-    ) {
-      decision<-round(runif(1,min=1,max=res.ratio[i,"some.ID"]))
-      if (decision<=res.ratio[i,"all.ID"]){
-        next
-      } else if (decision<=res.ratio[i,"non.ID"]){
-        new.tax<-taxa[taxa$full.taxon==n,]
-        new.tax[,i]<-"NA"
-        new.tax$full.taxon<-paste0(new.tax[,colnames(new.tax)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")], collapse="-")
-        taxa<-rbind(taxa,new.tax)
-        taxa<-taxa[taxa$full.taxon!=n,]
-        next
-      } else { #some.IF
-        numb.kept<-ceiling(runif(1,0.1,0.99)*taxa$count[taxa$full.taxon==n])
-        numb.upped<-taxa$count[taxa$full.taxon==n]-numb.kept
-        new.tax<-taxa[taxa$full.taxon==n,]
-        new.tax[,i]<-"NA"
-        new.tax$count<-numb.upped
-        new.tax$full.taxon<-paste0(new.tax[,colnames(new.tax)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")], collapse="-")
-        taxa$count[taxa$full.taxon==n]<-numb.kept
-        taxa<-rbind(taxa,new.tax)
-        next
+      taxa$full.taxon<-apply(taxa[,colnames(taxa)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")],
+                             1,paste0,collapse="-")
+      taxa1<-aggregate(count~full.taxon,data=taxa,sum)
+      taxa<-cbind(taxa1,do.call(rbind, strsplit(taxa1$full.taxon,"-")))
+      colnames(taxa)[3:8]<-c("n.p","n.c","n.o","n.f","n.g","n.s")
+      taxa[3:8]<-apply(taxa[3:8],2,as.character)
+      checked[i]<-T
+    } else {
+      for (n in unique(
+        taxa$full.taxon[taxa[,names(checked[which.max(checked)])]=="NA"]
+      )
+      ) {
+        if (taxa$count[taxa$full.taxon==n]==0) next
+        
+        decision<-round(runif(1,min=1,max=res.ratio[i,"some.ID"]))
+        if (decision<=res.ratio[i,"all.ID"]){
+          next
+        } else if (decision<=res.ratio[i,"non.ID"]){
+          new.tax<-taxa[taxa$full.taxon==n,]
+          new.tax[,i]<-"NA"
+          new.tax$full.taxon<-paste0(new.tax[,colnames(new.tax)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")], collapse="-")
+          taxa<-rbind(taxa,new.tax)
+          taxa<-taxa[taxa$full.taxon!=n,]
+          next
+        } else { #some.IF
+          numb.kept<-ceiling(runif(1,0.1,0.99)*taxa$count[taxa$full.taxon==n])
+          numb.upped<-taxa$count[taxa$full.taxon==n]-numb.kept
+          new.tax<-taxa[taxa$full.taxon==n,]
+          new.tax[,i]<-"NA"
+          new.tax$count<-numb.upped
+          new.tax$full.taxon<-paste0(new.tax[,colnames(new.tax)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")], collapse="-")
+          taxa$count[taxa$full.taxon==n]<-numb.kept
+          taxa<-rbind(taxa,new.tax)
+          next
+        }
       }
+      taxa$full.taxon<-apply(taxa[,colnames(taxa)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")],
+                             1,paste0,collapse="-")
+      taxa1<-aggregate(count~full.taxon,data=taxa,sum)
+      taxa<-cbind(taxa1,do.call(rbind, strsplit(taxa1$full.taxon,"-")))
+      colnames(taxa)[3:8]<-c("n.p","n.c","n.o","n.f","n.g","n.s")
+      taxa[3:8]<-apply(taxa[3:8],2,as.character)
+      checked[i]<-T
     }
-    taxa$full.taxon<-apply(taxa[,colnames(taxa)%in%c("n.p","n.c","n.o","n.f","n.g","n.s")],
-                           1,paste0,collapse="-")
-    taxa1<-aggregate(count~full.taxon,data=taxa,sum)
-    taxa<-cbind(taxa1,do.call(rbind, strsplit(taxa1$full.taxon,"-")))
-    colnames(taxa)[3:8]<-c("n.p","n.c","n.o","n.f","n.g","n.s")
-    taxa[3:8]<-apply(taxa[3:8],2,as.character)
-    checked[i]<-T
   }
+  
+  rem.coms[[x]]<-taxa
 }
 
-#sum(taxa$count)
-#sum(taxa.whole$count)
+rem.out<-lapply(rem.coms,"[[",'count')
+rem.out<-lapply(rem.out,data.frame)
+rem.out.names<-lapply(rem.coms,"[[",'full.taxon')
+for (x in 1:30){
+  row.names(rem.out[[x]])<-rem.out.names[[x]]
+}
+rem.out<-lapply(rem.out,t)
+rem.out<-lapply(rem.out,data.frame)
 
-taxa.mod<-taxa
+rem.df<-do.call(rbind.fill, rem.out)
+
+rem.df[is.na(rem.df)]<-0
+ref.df[is.na(ref.df)]<-0
+
+rownames(rem.df)<-paste0("rem",1:30)
+rownames(ref.df)<-paste0("ref",1:30)
+
+all.df<-rbind.fill(ref.df,rem.df)
+rownames(all.df)<-c(paste0("ref",1:30),paste0("rem",1:30))
+all.df[is.na(all.df)]<-0
+
+all.df<-all.df[,colSums(all.df)>0]
+
+ordiplot(vegan::cca(log10(all.df+1),scale=T),type="t",scaling="symmetric")
+
 
 ###################
 #  Calculate index of dataset completeness
